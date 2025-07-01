9c6c99aa4293f98ce2bf205c8364bfb3a88240af
Lakshmi-Boora <booralakshmi18@gmail.com>
2025-07-01 20:31:45 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const inputBox=document.getElementById("input-box")
 const listContainer=document.getElementById("list-container")
-
 function addTask(){
     if(inputBox.value=="")
         alert("you must write something...")
     else{
         let li=document.createElement("li");
         li.innerText=inputBox.value
-        listContainer.appendChild(li)
         let span=document.createElement("span")
         span.innerHTML="\u00d7";
         li.appendChild(span)
+        listContainer.appendChild(li)
     }
     inputBox.value=" "
     saveData();
@@ -31,4 +30,5 @@ function saveData(){
 function showTask(){
     listContainer.innerHTML=localStorage.getItem("data");
 }
+// localStorage.remove()
 showTask();
